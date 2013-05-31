b2389393bdfbaf3f6da1c0aa9d1b16bb11301118
Sébastien Fragnaud <sebastien.fragnaud@expandium.com>
2013-05-31 15:16:18 +0200

    Gna

--- js/app.js
+++ js/app.js
@@ -12,20 +12,38 @@ angular.module('resume', [])
 .controller('MenuCtrl', function($scope, $location) {
   $scope.items = [
     {value: '/home', label: 'Home'},
-    {value: '/experience', label: 'Experience'},
-    {value: '/contact', label: 'Contact'}
+    {value: '/experience', label: 'Experience'}
   ];
   
   $scope.getClass = function(item) {
-    if ($location.path() == item.value) {
-      return "pure-menu-selected"
-    } else {
-      return ""
-    }
+    return $location.path() == item.value ? "pure-menu-selected" : "pure-menu-non-selected";
   }
 })
 
-.controller('ExperienceCtrl', function() {
+.controller('ContactCtrl', function($scope) {
   
+})
+
+.controller('ExperienceCtrl', function($scope) {
+  $scope.experiences = [
+    {
+      what: 'Front-end developer',
+      when: 'since january 2011',
+      how: "Built three versions of the graphical interface of a cellular networks (both railways and mobile) monitoring solution. I've successfully solved several data visualization issues in a creative, user-centric way : representing technical data linked to a phone call, for instance, which can not be achieved in a classical chart but must be friendlier than a regular grid.",
+      tags: ['JavaScript', 'HTML5', 'Qooxdoo', 'D3.js', 'CSS', 'LESS', 'Python', 'Flex', 'AngularJS']
+    },
+    {
+      what: 'Flex developer',
+      when: 'from 2008 to 2011',
+      how: 'Build a three dimensional chart for mathematical data visualization. Mostly worked on a Matlab-like Rich Internet Application, and on internal tools such as an Air-based icon builder.',
+      tags: ['Flex', 'ActionScript', 'XML', 'Flex Charts']
+    },
+    {
+      what: "Engineer's degree",
+      when: 'from 2003 to 2008',
+      how: 'Degree from a generalist engineering school. Learned disciplins such as physics, thermodynamics, industrial electricy, automatisms, electronics...',
+      tags: []
+    }
+  ];
 })
 ;
