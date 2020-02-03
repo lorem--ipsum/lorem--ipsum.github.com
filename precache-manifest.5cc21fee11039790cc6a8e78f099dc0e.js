528100ec3696d6ef147ed11d902300560fb70293
Sébastien <sebastien@imply.io>
2020-02-03 18:35:27 +0100

    Updates

---
+++ precache-manifest.5cc21fee11039790cc6a8e78f099dc0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3673a75c4bafc8efbef665314a2738",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a197b604abb66a33d02d",
+    "url": "/static/css/main.db24d0d6.chunk.css"
+  },
+  {
+    "revision": "fd215eb1e24dcbe4f762",
+    "url": "/static/js/2.a0b6f69a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a0b6f69a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a197b604abb66a33d02d",
+    "url": "/static/js/main.033c7436.chunk.js"
+  },
+  {
+    "revision": "adf4c818b42be699b26c",
+    "url": "/static/js/runtime-main.27436ee6.js"
+  },
+  {
+    "revision": "b01b9edc335a5673dcd49a98916e659b",
+    "url": "/static/media/avatar.b01b9edc.jpeg"
+  }
+]);
